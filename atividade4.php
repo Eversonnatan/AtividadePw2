8d36f1b2d390e80b0c732e691687df4497ee8516
Eversonnatan <106198940+Eversonnatan@users.noreply.github.com>
2022-09-20 16:15:42 -0300

    Add files via upload

---
+++ atividade4.php
@@ -0,0 +1,23 @@
+<?php
+$valor1=20;
+$valor2=10;
+$valor3=2;
+$ordem1= $valor3 > $valor2 && $valor3 > $valor1;
+$ordemn2= $valor2 < $valor3 && $valor2 > $valor1;
+$ordem3=  $valor1 < $valor2 && $valor1 < $valor3;
+
+echo $ordem1;
+echo "Os números são: ".$valor1 .", " .$valor2. "  e ".$valor3
+
+///if ($ordem1= $valor3 > $valor2 && $valor3 > $valor1 ){
+//    echo $valor1;  //1
+//}
+//elseif( $ordemn2= $valor2 < $valor3 && $valor2 > $valor1){
+
+//}
+//else($ordem3=  $valor1 < $valor2 && $valor1 < $valor3)
+
+
+
+
+?>
